24b78f9f2de269a9995d053e7b6c2ac7075174b4
Pablo QB <qb.pablo.2000@gmail.com>
2020-06-15 10:19:33 +0200

    Initial commit

---
+++ eventListeners.js
@@ -0,0 +1,32 @@
+function mouseDragged() {
+	UI.mouseDragged();
+	Drag.mouseDragged();
+}
+
+function mousePressed() {
+	UI.mousePressed();
+	Drag.mousePressed();
+}
+
+function mouseClicked() {
+    UI.mouseClicked();
+	Drag.mouseClicked();
+}
+
+function mouseReleased() {
+    UI.mouseReleased();
+	Drag.mouseReleased();
+}
+
+function mouseWheel(event) {
+	UI.mouseWheel(event);
+}
+
+function keyPressed() {
+  UI.keyPressed();
+}
+
+function keyTyped() {
+	UI.keyTyped();
+}
+
